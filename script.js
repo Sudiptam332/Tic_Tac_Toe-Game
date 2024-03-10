d46aec14619513232c6d0ec2219daec3ff54f91f
Sudipta Mondal <76097232+Sudiptam332@users.noreply.github.com>
2024-03-10 19:01:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+let boxs = document.querySelectorAll(".box");
+let personX = true;
+
+const winCondition = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+]
+
+boxs.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(personX){
+            box.innerText = "X";
+            personX = false;
+            box.style.color = "red";
+        }
+        else{
+            box.innerText = "O";
+            personX = true;
+        }
+        box.disabled = true;
+
+        checkWiner();
+    });
+});
+
+let found = false;
+const checkWiner = () => {
+    for(stbox of winCondition){
+        let b1 = boxs[stbox[0]].innerText;
+        let b2 = boxs[stbox[1]].innerText;
+        let b3 = boxs[stbox[2]].innerText;
+        if(b1 !== "" && b2 !== "" && b3 !== ""){
+            if(b1 === b2 && b2 === b3){
+                console.log(`win ${b1}`);
+                showWiner(b1);
+                found = true;
+            }
+        }
+    }
+}
+
+let msg = document.querySelector("#msg");
+let newGame = document.querySelector("#newGame");
+let msgCon = document.querySelector(".newMsg");
+let clear = document.querySelector(".clear");
+let game = document.querySelector(".game");
+
+const disableBox = () => {
+    for(let box of boxs){
+        box.disabled = true;
+    }
+}
+
+const enableBox = () => {
+    for(let box of boxs){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWiner = (w) => {
+    msg.innerText = `Congratulations, '${w}' Win, Start a New Game.`;
+    msgCon.classList.remove("hide");
+    game.classList.add("hide");
+    clear.classList.add("hide");
+    disableBox();
+}
+
+const draw = () => {
+    msg.innerText = `It's Draw, Start a New Game.`;
+    msgCon.classList.remove("hide");
+    game.classList.add("hide");
+    clear.classList.add("hide");
+    disableBox();
+}
+
+const resetGame = () => {
+    personX = true;
+    enableBox();
+    msgCon.classList.add("hide");
+    game.classList.remove("hide");
+    clear.classList.remove("hide");
+}
+
+newGame.addEventListener("click", resetGame);
+clear.addEventListener("click", resetGame);
